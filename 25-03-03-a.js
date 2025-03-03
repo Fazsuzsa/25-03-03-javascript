b3ef7f68d2a2586490f663075c359638b6e0ef45
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-03 12:23:36 +0100

    Create 25-03-03-a.js

---
+++ 25-03-03-a.js
@@ -0,0 +1,36 @@
+// Kommentar
+console.log("Hello World")
+
+// Variablen:
+let my_name = "Zsuzsa";
+let alter = 32;
+
+console.log(`Ich bin ${my_name}.`); // formattierter Strings
+console.log(alter);
+console.log(my_name + " " + alter);
+
+let b;
+console.log(b);
+
+// primitive Datentypen:
+a = 5           // number
+a = true        // boolean
+a = "Text"      // string
+a = undefined   // undefined ("Noch kein Wert")
+a = null        // null ("Absichtlich kein Wert")
+
+// primitive Rechenoperationen:
+let x = 2;
+let y = 4;
+console.log(x + y);
+console.log(x - y);
+console.log(x * (y + y));
+console.log(x / y);
+console.log(0.1 + 0.2);
+console.log((0.1 + 0.2).toFixed(2));
+
+let hochzahl = Math.pow(x, y);
+console.log(hochzahl);
+
+let quadratwurzel = Math.sqrt(y);
+console.log(quadratwurzel);
