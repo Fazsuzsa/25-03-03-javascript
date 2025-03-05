22c766d9426e81eac2c993d367bb8548bab84e47
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-05 12:57:10 +0100

    update

---
+++ 25-03-05-a/index.js
@@ -0,0 +1,16 @@
+function onClickButton() {
+  const boxDiv = document.getElementById("box");
+  boxDiv.innerHTML = "<button>I am a button</button>";
+  const currentBgColor = boxDiv.style.backgroundColor;
+  console.log("box div background", currentBgColor);
+  boxDiv.style.backgroundColor = "green";
+}
+
+function onLogin() {
+  const emailTI = document.getElementById("emailti").value;
+  const password = document.getElementById("passwordti").value;
+  console.log("My Credentials", emailTI, password);
+  alert(
+    `Der Benutzer hat sich erfolgreich mit ${emailTI} und ${password} angemeldet`
+  );
+}
