ee6125be3daa03ae588963ea9d396cae557e7c7e
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-06 20:01:44 +0100

    Create 25-03-06-ha.js

---
+++ 25-03-06-ha.js
@@ -0,0 +1,11 @@
+// jshero 
+
+// 31. Modulo
+// Another arithmetic operator is modulo. 
+// It calculates the remainder of a division and is represented by %.
+// Write a function onesDigit that takes a natural number and returns the ones digit of that number.
+function onesDigit(number) {
+    return number % 10
+}
+
+console.log(onesDigit(2674))
