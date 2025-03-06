406743745a32dc6c985cad0a1ef8cbc13638a2de
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-06 09:52:02 +0100

    Update signup.js

--- 25-03-05-ha/signup.js
+++ 25-03-05-ha/signup.js
@@ -1,3 +1,11 @@
+function onClickButton() {
+  const boxDiv = document.getElementById("box");
+  boxDiv.innerHTML = "<button>I am a button</button>";
+  const currentBgColor = boxDiv.style.backgroundColor;
+  console.log("box div background", currentBgColor);
+  boxDiv.style.backgroundColor = "green";
+}
+
 function userSignUp() {
   const emailTi = document.getElementById("emailti").value;
   const passwordTi = document.getElementById("passwordti").value;
@@ -5,18 +13,18 @@ function userSignUp() {
   const firstName = document.getElementById("firstName").value;
   const familyName = document.getElementById("familyName").value;
 
-  if (emailTi.length >= 3) {
-    if (passwortTi === rePasswordTi) {
-      if (firstName && familyName !== undefined) {
+  if (emailTi && passwordTi && rePasswordTi && firstName && familyName) {
+    if (emailTi.length >= 3) {
+      if (passwordTi === rePasswordTi) {
         alert(`Benutzer ${firstName} ${familyName} hat sich erfolgreich mit ${emailTi} und ${passwordTi} angemeldet`)
       } else {
-        alert("Nicht alles ausgefüllt!")
+        alert("Die Passwörter stimmen nicht überein!")
       }
     } else {
-      alert("Die Passwörter stimmen nicht überein!")
+      alert("Email ist kürzer als 3 Zeichen!")
     }
   } else {
-    alert("Email ist kürzer als 3 Zeichen!")
+    alert("Nicht alles ausgefüllt!")
   }
 }
 
