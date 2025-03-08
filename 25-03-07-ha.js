8e0842661056b27e1a746762670fc14ad458b430
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-08 11:03:30 +0100

    Update 25-03-07-ha.js

--- 25-03-07-ha.js
+++ 25-03-07-ha.js
@@ -1,11 +1,20 @@
-// find() method
+// find()
 // Returns the value of the first element in an array that pass a test
 
 const RANDOMNUMBERS = [{nr: 8}, {nr: 4}, {nr: 5}, {nr: 4}];
 const CURRENTITEM = RANDOMNUMBERS.find((item) => item.nr === 4);
 console.log("Current item:", CURRENTITEM);
 
-// filter() method
+// findIndex()
+// Returns the index of the first element in an array that pass a test
+
+// findLast()
+// Returns the value of the last element in an array that pass a test
+
+// findLastIndex()
+// Returns the index of the last element in an array that pass a test
+
+// filter()
 // Creates a new array with every element in an array that pass a test
 
 const ages = [16, 18, 20, 32];
@@ -17,7 +26,7 @@ function checkAdult(age) {
 
 console.log(adult)
 
-// concat() method
+// concat()
 // Joins arrays and returns an array with the joined arrays
 
 const arr1 = ["Cecilie", "Lone"];
@@ -28,7 +37,7 @@ const children3 = arr1.concat(arr2, arr3);
 console.log(children2)
 console.log(children3)
 
-// flat() method
+// flat()
 // Concatenates sub-array elements
 
 const myArr = [[1,2],[3,4],[5,6]];
@@ -39,12 +48,13 @@ const myArr2 = [1, 2, [3, [4, 5, 6], 7], 8];
 const newArr2 = myArr2.flat(2);
 console.log(newArr2)
 
-// forEach() method
+// forEach()
 // Calls a function for each array element
 
-// ??
+// includes()
+// Check if an array contains the specified element
 
-// indexOf() method
+// indexOf()
 // Search the array for an element and returns its position
 
 const fruits = ["Banana", "Orange", "Apple", "Mango", "Banana", "Orange", "Apple", "Mango"];
@@ -53,4 +63,32 @@ console.log(index)
 let index2 = fruits.indexOf("Apple", 3);
 console.log(index2)
 
+// join()
+// Joins all elements of an array into a string
 
+// length
+// Sets or returns the number of elements in an array
+
+// map()
+// Creates a new array with the result of calling a function for each array element
+
+// pop()
+// Removes the last element of an array, and returns that element
+
+// push()
+// Adds new elements to the end of an array, and returns the new length
+
+// reduce()
+// Reduce the values of an array to a single value (going left-to-right)
+
+// slice()
+// Selects a part of an array, and returns the new array
+
+// sort()
+// Sorts the elements of an array
+
+// splice()
+// 	Adds or Removes array elements
+
+// toString()
+// Adds or Removes array elements
