8da629af1bc7c5f5be0dda67b6bc6a762cbbe0a2
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-10 22:01:57 +0100

    update

--- 25-03-10-ha/25-03-10-ha.js
+++ 25-03-10-ha/25-03-10-ha.js
@@ -10,33 +10,45 @@ function createHTMLList(liste){
 let lehrer = ["Lukas", "Tom", "Kevin", "Suheib"];
 
 function setListContent(){
-    let listDiv = document.getElementById("liste")
-    let content = createHTMLList(lehrer)
-    listDiv.innerHTML = content
+    let listDiv = document.getElementById("liste");
+    let content = createHTMLList(lehrer);
+    listDiv.innerHTML = content;
 }
 
 function setUserInputList(){
     // Text und Div aus dem Dokument holen:
-    let userInput = document.getElementById("userInput")
-    let text = userInput.value
-    let listDiv = document.getElementById("liste")
-    let textList = text.split(",")
-    let content = createHTMLList(textList)
-    listDiv.innerHTML = content
+    let userInput = document.getElementById("userInput");
+    let text = userInput.value;
+    let listDiv = document.getElementById("liste");
+    let textList = text.split(",");
+    let content = createHTMLList(textList);
+    listDiv.innerHTML = content;
 }
 
 function setUserInputList2(){
-    let userInput = document.getElementById("userInput2")
-    let text = userInput.value
-    let textList = text.split(",")
-    lehrer = textList
-    setListContent()
+    let userInput = document.getElementById("userInput2");
+    let text = userInput.value;
+    let textList = text.split(",");
+    lehrer = textList;
+    setListContent();
 }
 
 function setUserInputList3(){
-    let userInput = document.getElementById("userInput3")
-    let text = userInput.value
-    let textList = text.split(",")
-    lehrer = lehrer.concat(textList)
-    setListContent()
+    let userInput = document.getElementById("userInput3");
+    let text = userInput.value;
+    let textList = text.split(",");
+    lehrer = lehrer.concat(textList);
+    setListContent();
+}
+
+function addItem(){
+    let userInput = document.getElementById("userInput4");
+    let newItem = userInput.value;
+    lehrerNeu = lehrer.push(newItem);
+    setListContent();
+}
+
+function removeLastItem(){
+    lehrer.pop();
+    setListContent();
 }
