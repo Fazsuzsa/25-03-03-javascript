22a9aeb01f58af4e553d2a62e54363bf462ede29
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-11 20:20:55 +0100

    update

---
+++ 25-03-11-ha/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+app.get("/content", (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Content</title>
+        </head>
+        <body>
+            <h1>Express</h1>
+            <p1>Hier wird HTML zurückgegeben.</p1>
+        </body>
+        </html>
+    `);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(3000, () => {
+    console.log("Server läuft auf Port 3000");
+});
