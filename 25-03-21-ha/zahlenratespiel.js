68c40c31055ca401ace194768e20766a0ebfab51
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-21 23:44:17 +0100

    update

--- 25-03-21-ha/zahlenratespiel.js
+++ 25-03-21-ha/zahlenratespiel.js
@@ -1,18 +1,15 @@
-let number = parseInt(prompt("Enter a guess between 1 and 10:"));
+let number = parseInt(document.getElementById("inputField").value);
 let randomNumber = Math.floor(Math.random() * 10) + 1;
 
 function guessingGame() {
+  let outputField = document.getElementById("outputField");
   if (number === randomNumber) {
-    alert("The guessed number is exactly right!");
+    outputField.textContent = "The guessed number is exactly right!";
   } else if (number < randomNumber && number > 0) {
-    alert(
-      `${randomNumber} is the right number. The guessed number is too low!`
-    );
+    outputField.textContent = `${randomNumber} is the right number. The guessed number is too low!`;
   } else if (number > randomNumber && number < 11) {
-    alert(
-      `${randomNumber} is the right number. The guessed number is too high!`
-    );
+    outputField.textContent = `${randomNumber} is the right number. The guessed number is too high!`;
   } else {
-    alert("Invalid number!");
+    outputField.textContent = "Invalid number!";
   }
 }
