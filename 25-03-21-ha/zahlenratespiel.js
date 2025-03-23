334d9fd38398b1900ab782003f1d4828be32ecd1
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-23 22:40:20 +0100

    update

--- 25-03-21-ha/zahlenratespiel.js
+++ 25-03-21-ha/zahlenratespiel.js
@@ -4,7 +4,8 @@ function guessingGame() {
   let number = parseInt(document.getElementById("inputField").value);
   let outputField = document.getElementById("outputField");
   if (number === randomNumber) {
-    outputField.textContent = "The guessed number is exactly right!";
+    outputField.textContent =
+      "Congratulations! The guessed number is exactly right!";
   } else if (number < randomNumber && number > 0) {
     outputField.textContent = `${randomNumber} is the right number. The guessed number is too low!`;
   } else if (number > randomNumber && number < 11) {
