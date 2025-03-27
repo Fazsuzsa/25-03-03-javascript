89dcf0cb583796d7a16d1968e7b582865611f8ee
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-03-28 00:31:36 +0100

    Update index.js

--- 25-03-27-ha/index.js
+++ 25-03-27-ha/index.js
@@ -10,9 +10,23 @@ app.get("/tiere", (req, res) => {
   res.json(tiere);
 }); // Gibt eine Liste aller Tiere zurück
 
-// app.get("/tiere/search", (req, res) => {
-//   }); // Filtert nach einer Tierart (z. B. Hund, Katze)
-//GET /tiere/:id // Gibt nur das Tier mit der angegebenen ID zurück
+app.get("/tiere/search", (req, res) => {
+  const art = req.query.art;
+  const result = tiere.filter((tier) => tier.art == art);
+  res.json(result);
+}); // Filtert nach einer Tierart (z. B. Hund, Katze)
+
+app.get("/tiere/:id", (req, res) => {
+  const id = req.params.id;
+  const foundAnimal = tiere.find((tier) => tier.id == id);
+
+  if (foundAnimal) {
+    res.json(foundAnimal);
+  } else {
+    res.status(404).send(`Tier mit der ID ${id} nicht gefunden.`);
+  }
+}); // Gibt nur das Tier mit der angegebenen ID zurück
+
 //POST /tiere // Fügt ein neues Tier zur Liste hinzu (über JSON-Body)
 
 app.listen(3000, () => {
