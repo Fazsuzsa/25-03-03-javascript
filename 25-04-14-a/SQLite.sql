e7c8a2a1a42cf84abd3d1ad7fe3181c152a4ecb6
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-04-14 15:57:40 +0200

    create

---
+++ 25-04-14-a/SQLite.sql
@@ -0,0 +1,19 @@
+-- SQLite
+
+INSERT INTO tiere(tierart,name,krankheit,age,gewicht) 
+VALUES ("Katze","Paw","schnupfen",5, 7.1);
+
+INSERT INTO tiere (tierart, name, krankheit, age, gewicht)
+VALUES ("Hund", "Chase", "zecken", 7, 14.8);
+
+INSERT INTO tiere (tierart, name, krankheit, age, gewicht)
+VALUES ("Papagei", "Jack", "federallergie", 2, 0.9);
+
+INSERT INTO tiere (tierart, name, krankheit, age, gewicht)
+VALUES ("Meerschweinchen", "Ham", "gesund", 1, 0.75);
+
+INSERT INTO tiere (tierart, name, krankheit, age, gewicht)
+VALUES ("Pferd", "Esel", "kolik", 10, 550);
+
+SELECT * FROM tiere
+
