cb533a55b698c84f51f16ec61d7c2efa95a8dd5f
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-04-16 16:59:41 +0200

    create

--- 25-04-15-ha/public/script.js
+++ 25-04-15-ha/public/script.js
@@ -1,110 +1,63 @@
-const buttonShowAnimals = document.getElementById("buttonShowAnimals")
-const showAnimals = document.getElementById("showAnimals")
-const addTierForm = document.getElementById("addTierForm")
-const showAddAnimalForm = document.getElementById("showAddAnimalForm")
-const content = document.getElementById("content")
-
-// ##################################################################
-
-// Button  um das Formular anzuzeigen
-
-showAddAnimalForm.addEventListener("click", () => {
-    addTierForm.hidden = false;
-    content.hidden = true;
-})
-
-// ##################################################################
-
-// Alle Tiere Anzeigenb lassen 
+const buttonShowAnimals = document.getElementById("button_show_animals");
+const showAnimals = document.getElementById("show_animals");
 
 buttonShowAnimals.addEventListener("click", async () => {
-    const res = await fetch("http://127.0.0.1:3000/tiere")
-    displayData(await res.json())
+  const res = await fetch("http://127.0.0.1:3000/tiere");
+  displayData(await res.json());
 
-    function displayData(data) {
-        console.log(data)
-        showAnimals.innerHTML = "";
-
-        // Wir starten eine Schleife für jeden Eintrag aus der Datenbank
-        data.forEach(tier => {
-            console.log(tier)
-
-            // erst wird das Zeilenelement für die Tabelle erstellt
-            const row = document.createElement("tr");
-
-            const tierart = document.createElement("td")
-            tierart.textContent = tier.tierart;
-            row.appendChild(tierart);
-
-            
-            const name = document.createElement("td")
-            name.textContent = tier.name;
-            row.appendChild(name);
-
-
-            const krankheit = document.createElement("td")
-            krankheit.textContent = tier.krankheit;
-            row.appendChild(krankheit);
-
-
-            const alter = document.createElement("td")
-            alter.textContent = tier.age;
-            row.appendChild(alter);
-
-
-            const gewicht = document.createElement("td")
-            gewicht.textContent = tier.gewicht;
-            row.appendChild(gewicht);
-
-            const deleteButtonFeld = document.createElement("td");
-
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Löschen"
-            deleteButton.addEventListener("click", () => deleteAnimal(tier.id))
-
-            deleteButtonFeld.appendChild(deleteButton)
-            row.appendChild(deleteButtonFeld);
-
-            showAnimals.appendChild(row);
-
-        });
-    }
-
+  function displayData(data) {
+    console.log(data);
+    showAnimals.innerHTML = "";
+    data.forEach((tier) => {
+      console.log(tier);
+      const li = document.createElement("li");
+      li.textContent = tier.name;
+      showAnimals.appendChild(li);
+    });
+  }
 });
 
-// ##################################################################
+const tierart = document.getElementById("inputTierart");
+const name = document.getElementById("inputName");
+const krankheit = document.getElementById("inputKrankheit");
+const age = document.getElementById("inputAge");
+const gewicht = document.getElementById("inputGewicht");
+const antwort = document.getElementById("antwort");
+const buttonAddAnimal = document.getElementById("button_add_animal");
 
-// Formular auslesen und POST senden
-
-addTierForm.addEventListener("submit", async (event) => {
-    event.preventDefault()
-
-    // Alle Inputfelder auswählen und ein Object erstellen
-
-    const newAnimal = {};
-    const inputs = addTierForm.querySelectorAll("input[name]")
-    inputs.forEach((input) => {
-        newAnimal[input.name] = input.value
+buttonAddAnimal.addEventListener("click", async () => {
+  try {
+    const requestBody = {
+      tierart: tierart.value,
+      name: name.value,
+      krankheit: krankheit.value,
+      age: age.value,
+      gewicht: gewicht.value,
+    };
+    fetch("http://localhost:3000/tiere", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
     })
+      .then((res) => res.json())
+      .then((data) => {
+        antwort.innerText = JSON.stringify(data);
+        console.log(data);
+      });
+  } catch (err) {
+    console.log("Fehler bei Fetch: "err);
+  }
+});
 
-    await fetch("http://127.0.0.1:3000/tiere", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newAnimal)
+window.onload = () => {
+  fetch("http://localhost:3000/tiere")
+    .then((res) => res.json())
+    .then((data) => {
+      antwort.innerText = JSON.stringify(data);
     });
-    
-    addTierForm.reset();
-    buttonShowAnimals.click()
-    addTierForm.hidden = true;
-    content.hidden = false;
-})
+};
 
-
-// ##################################################################
-
-// löschfunktion
+// Löschfunktion
 
 async function deleteAnimal(id) {
     await fetch(`http://127.0.0.1:3000/tiere/${id}`, {
