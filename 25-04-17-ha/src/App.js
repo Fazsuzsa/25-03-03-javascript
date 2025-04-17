cdaf0133ff75e1ec7a5bb426e3cbeee54ddeb8ce
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-04-17 15:42:05 +0200

    create

--- 25-04-17-ha/src/App.js
+++ 25-04-17-ha/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  let initialColour = "darkolivegreen";
+  const [colour, setColour] = useState(initialColour);
+  function changeToInitial() {
+    setColour(initialColour);
+  }
+
+  const [counter, setCount] = useState(0);
+  function clickCount() {
+    setCount(counter + 1);
+  }
+
+  useEffect(() => {
+    console.log("Hallo, ich komme aus dem UseEffect!");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ backgroundColor: colour }}>Hausaufgabe 16.04.2025</h1>
+      <button onClick={() => setColour("purple")}>
+        Ändere die Farbe zu lila
+      </button>
+      <button onClick={changeToInitial}>Ändere die Farbe zurück</button>
+      <br />
+      <button onClick={clickCount}>Du hast {counter}-mal geklickt!</button>
+    </>
   );
 }
 
