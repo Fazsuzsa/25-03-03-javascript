ac22641f5fcbf553d960396d8b0e51d9383d0f29
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-04-17 13:44:33 +0200

    create

--- 25-04-17-a/src/App.js
+++ 25-04-17-a/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  let startZahl = 10;
+  const [zahl, setZahl] = useState(startZahl);
+  function reset() {
+    setZahl(startZahl);
+  }
+
+  let initialColour = "wheat";
+  const [colour, setColour] = useState(initialColour);
+  function changeToInitial() {
+    setColour(initialColour);
+  }
+
+  useEffect(() => {
+    console.log("Hallo, ich komme aus dem UseEffect!");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Meine Lieblingszahl ist: {zahl}.</p>
+      <button onClick={() => setZahl(zahl + 1)}>+</button>
+      <button onClick={() => setZahl(zahl - 1)}>-</button>
+      <button onClick={reset}>R</button>
+
+      <h1 style={{ color: colour }}>Ich bin eine Überschrift</h1>
+      <button onClick={() => setColour("purple")}>
+        Ändere die Farbe zu lila
+      </button>
+      <button onClick={changeToInitial}>Ändere die Farbe zurück</button>
+    </>
   );
 }
 
