48afe0aa0079e1058e5f60b5923a0e0a1c608acd
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-04-25 19:39:25 +0200

    update

--- 25-04-25-ha/frontend/src/App.js
+++ 25-04-25-ha/frontend/src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 function App() {
   const [animals, setAnimals] = useState([]);
